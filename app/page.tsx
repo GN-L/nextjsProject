2a0dedc298838ee10d8fc4cca68f96f45527e27e
lgn <913046425@qq.com>
2025-09-09 14:02:57 +0800

    fix: 字体资源

--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,7 @@ import Link from 'next/link';
 import styles from '@/app/ui/home.module.css';
 import OrderStatus from '@/app/ui/invoices/status';
 import Image from 'next/image';
+import { lusitana } from '@/app/ui/fonts';
 
 export default function Page() {
   return (
@@ -17,7 +18,7 @@ export default function Page() {
           <div
   className={styles.shape}
 />
-          <p className={`text-xl text-gray-800 md:text-3xl md:leading-normal`}>
+          <p className={`${lusitana.className} text-xl text-gray-800 md:text-3xl md:leading-normal`}>
             <strong>Welcome to Acme.</strong> This is the example for the{' '}
             <a href="https://nextjs.org/learn/" className="text-blue-500">
               Next.js Learn Course
